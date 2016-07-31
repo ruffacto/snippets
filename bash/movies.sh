1a610a4b15e608f53854e3c4f3cfd7f810ef46a6
Ruben Hernandez <ruffact@gmail.com>
2016-07-31 00:30:18 -0700

    Some necessary adjustments, after testing

--- bash/movies.sh
+++ bash/movies.sh
@@ -2,21 +2,19 @@
 export makemkvcon=/Applications/MakeMKV.app/Contents/MacOS/makemkvcon
 export HandBrakeCLI=~/Documents/scripts/HandBrakeCLI/HandBrakeCLI
 decrypt_and_backup(){  # make a full decrypted disc backup
-  mountedDiscFolder=${1:-NONO}
-  # your new Movie Folder in $2
+  mountedDiscNumber=${1:-NONO}
   newMovieFolder=${2:-NONO}
   if [ "$newMovieFolder" = "NONO" -o "$mountedDiscFolder" = "NONO" ]
   then
-    echo "decrypt_and_backup <mountedDiscFolder> <newMovieFolder>"
-    echo "decrypt_and_backup /Volumes/MovieName ~/Movies/MovieName"
+    echo "decrypt_and_backup <mountedDiscNumber> <newMovieFolder>"
+    echo "decrypt_and_backup 2 ~/Movies/MovieName"
   else
     echo "should not see this"
-    ${makemkvcon} --decrypt backup disc:${mountedDiscFolder} ${newMovieFolder}
+    ${makemkvcon} --decrypt backup disc:${mountedDiscNumber} ${newMovieFolder}
   fi
 }
 
 print_info(){
-  # your Movie Folder in $1
   MovieFolder=${1:-NONO}
   if [ "$MovieFolder" = "NONO" ]
   then
@@ -28,15 +26,15 @@ print_info(){
 }
 
 create_mkv(){
-  # your Movie Folder in $1
   MovieFolder=${1:-NONO}
-  MKVFolder=${2:-NONO}
-  if [ "$MovieFolder" = "NONO" -o "$MKVFolder" = "NONO" ]
+  TitleNumber=${2:-NONO}
+  MKVFolder=${3:-NONO}
+  if [ "$MovieFolder" = "NONO" -o "$MKVFolder" = "NONO" -o "$TitleNumber" = "NONO" ]
   then
-    echo "create_mkv <MovieFolder> <MKVFolder>"
-    echo "create_mkv  ~/Movies/MovieName ~/Movies"
+    echo "create_mkv <MovieFolder> <TitleNumber> <MKVFolder>"
+    echo "create_mkv  ~/Movies/MovieName 2 ~/Movies"
   else
-    ${makemkvcon} --cache=1024 -r mkv file:${MovieFolder} 0 /Users/ruffact/Movies
+    ${makemkvcon} --cache=1024 -r mkv file:${MovieFolder} ${TitleNumber} ${MKVFolder}
   fi
 }
 
@@ -50,16 +48,16 @@ azulMKV_mp4(){
     echo "azulMKV_mp4  ~/Movies/MovieName ~/Movies"
   else
 
-  for full_path_name in $(ls ${inPath}/*mkv 2>/dev/null)
-  do
-    full_name=$(basename $full_path_name)
-    name=${full_name%.*}
-    # name=BIG_BANG_S05_D01_E01
-    echo; echo Processing $name; echo
-    inFile=${name}.mkv
-    outFile=${name}.mp4
-    out="-o ${outPath}/${outFile}"
-    source="-i ${inPath}/${inFile} --subtitle 1 "
-    ${HandBrakeCLI} $source $out --preset="Android Tablet" --vfr
-  done
+    for full_path_name in $(ls ${inPath}/*mkv 2>/dev/null)
+    do
+      full_name=$(basename $full_path_name)
+      name=${full_name%.*}
+      echo; echo Processing $name; echo
+      inFile=${name}.mkv
+      outFile=${name}.mp4
+      out="-o ${outPath}/${outFile}"
+      source="-i ${inPath}/${inFile} --subtitle 1 "
+      ${HandBrakeCLI} $source $out --preset="Android Tablet" --vfr
+    done
+  fi
 }
